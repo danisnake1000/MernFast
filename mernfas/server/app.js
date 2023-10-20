757c67516e5b421b80f4100e5b1b6e6b4c820291
danisnake <adriandanielfernandez@gmail.com>
2023-10-19 22:41:14 -0300

    prueba

--- mernfas/server/app.js
+++ mernfas/server/app.js
@@ -1,9 +1,14 @@
 
 import express, { json } from 'express';
-import {router} from './routes/posts.routes.js' 
+import {router} from './routes/posts.routes.js'
+import fileUpload from 'express-fileupload'; 
 const app = express();
 
 app.use(express.json())
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir:'.uploads'
+}))
 app.use(router)
 
 
--- mernfas/server/controllers/posts.controllers.js
+++ mernfas/server/controllers/posts.controllers.js
@@ -1,28 +1,57 @@
 import Posts from "../models/Posts.js";
 
 export const getPosts = async (req, res) => {
-  const posts = await Posts.find();
-  res.send(posts);
+  try {
+    const posts = await Posts.find();
+    res.send(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const createPosts = async (req, res) => {
-  const { title, description } = req.body;
-  const post = new Posts({ title, description });
-  await post.save();
-  return res.json(post);
+  try {
+    const { title, description } = req.body;
+    if(req.file.image){
+      
+    }
+    const post = new Posts({ title, description });
+    await post.save();
+    return res.json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-export const getPostsId = async (req, res) => {};
+export const getPostsId = async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res.sendStatus(404);
+    }
+    return res.json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const updatePosts = async (req, res) => {
-  const post = await Posts.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  return res.json(post);
+  try {
+    const updatedPost = await Posts.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    return res.json(updatePosts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deletePosts = async (req, res) => {
-    const postRemoved = await Posts.findByIdAndDelete(req.params.id)
-    if(!postRemoved) return res.send("nada eliminado")
+  try {
+    const postRemoved = await Posts.findByIdAndDelete(req.params.id);
+    if (!postRemoved) return res.send("nada eliminado");
     return res.json(postRemoved);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
