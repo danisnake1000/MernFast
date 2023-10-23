c5e32f0163c9b4ebe112eb15a2ea8acc25c3e49a
danisnake <adriandanielfernandez@gmail.com>
2023-10-22 22:25:46 -0300

    terminado

--- mernfas/server/db.js
+++ mernfas/server/db.js
@@ -1,6 +1,8 @@
 
 import mongoose from "mongoose";
 import {MONGODB_URI} from './config.js'
+
+
 export async function connectDB(){
     try {
         const db = await mongoose.connect(MONGODB_URI)
